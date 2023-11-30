eddb142dfe4e0f2126f6d16c1c4b81103ea70e96
katsuya.suda <katsuya.suda@keit.co.jp>
2023-11-30 17:20:47 +0900

    models修正 htmlファイル修正

---
+++ app1/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import (
+    ContentId, RequirementsInfometion,
+)
+
+### 検索条件フォーム ###
+### 現在index.htmlに直で記載している為、使用する場合、変更する必要あり
--- app1/models.py
+++ app1/models.py
@@ -11,11 +11,11 @@ class ContentId(models.Model):
     def __str__(self):
         return self.contentId
 
-#Create your models here.
-#### 条件情報 ####
+#region 条件情報
 class RequirementsInfometion(models.Model):
     #### 登録形態 ####
     registrationFormRadioButton = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False)
@@ -23,66 +23,76 @@ class RequirementsInfometion(models.Model):
     #### 出力先 ####
     #JTB
     outputDestinationJtbChackBox = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False)
     #トラベルコ
     outputDestinationTravelChackBox = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False)
     #パンフレット
     outputDestinationPamphletChackBox = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False)
 
     #### 出発地 ####
     departurePoint = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False)
     # 大人人数
-    adultNum = models.CharField(
-        max_length=255,
-        blank=False,
-        null=False)
+    adultNum = models.IntegerField(
+        verbose_name='',
+        blank=True,
+        null=True,
+        default=0)
 
     #### 出発美設定時間 ####
     #絶対設定
     departureDateSettingRadio = models.CharField(
+        verbose_name='絶対設定',
         max_length=255,
         blank=False,
         null=False)     
     #絶対設定_開始日付
-    absoluteSettingStartDate = models.CharField(
-        max_length=255,
+    absoluteSettingStartDate = models.DateTimeField(
+        verbose_name='',
         blank=False,
         null=False) 
     #絶対設定_終了日付
-    absoluteSettingEndDate = models.CharField(
-        max_length=255,
+    absoluteSettingEndDate = models.DateTimeField(
+        verbose_name='',
         blank=False,
         null=False) 
     
     #絶対設定_開始リスト
     relativeSettings1StartList = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False) 
     #絶対設定_終了リスト
     relativeSettings1EndList = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False) 
     
     #絶対設定_開始日エリア
     relativeSettings2StartDayTxt = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False) 
     #絶対設定_終了日エリア
     relativeSettings2EndDayTxt = models.CharField(
+        verbose_name='',
         max_length=255,
         blank=False,
         null=False) 
@@ -93,3 +103,4 @@ class RequirementsInfometion(models.Model):
     
     def __str__(self):
         return self.contentId
+#endregion 条件情報
